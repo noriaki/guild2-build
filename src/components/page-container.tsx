6348d9c729bd00d02ff40b5fa8266c14082c1684
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-09-21 20:13:44 +0900

    add next.js common implementation

---
+++ src/components/page-container.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import Router from 'next/router';
+
+Router.events.on('routeChangeComplete', (url) => console.log(url));
+
+const PageContainer = ({ title , children }) => (
+  <div>
+    <Head>
+      <title>{title || 'ギルド物語2キャラクター画像ジェネレーター'}</title>
+    </Head>
+    {children}
+  </div>
+);
+
+export default PageContainer;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PageContainer from '~/components/page-container';
+
+const IndexPage = () => (
+  <PageContainer>
+    <div>aa</div>
+  </PageContainer>
+);
+
+export default IndexPage;
