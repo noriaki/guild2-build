0f051ef966a2cc56516a81541305673a5b156f22
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-09-23 21:52:16 +0900

    add canvas components

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Stage, Layer, Text, Image } from 'react-konva';
+import { Stage, Layer, Image } from 'react-konva';
+import useImage from 'use-image';
 
-const Canvas = () => (
-  <Stage width={120} height={120}>
-    <Layer>
-      <Text text="吸" fronSize={15} />
-    </Layer>
-  </Stage>
-);
+import TextRect from './text-rect';
+
+const Canvas = () => {
+  const [image] = useImage('/icons/royalline.png');
+  return (
+    <Stage width={120} height={120}>
+      <Layer>
+        <Image image={image} />
+        <TextRect x={2} y={12} text="人(男)" />
+        <TextRect x={2} y={48} text="ロ(M)" />
+        <TextRect x={2} y={84} text="エ" />
+      </Layer>
+    </Stage>
+  );
+};
 
 export default Canvas;
