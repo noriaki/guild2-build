eccc1bae0d62b2ef4d75ab968698493d88f269d8
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-09-24 14:04:56 +0900

    fix: eslint error

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -4,7 +4,7 @@ import Router from 'next/router';
 
 Router.events.on('routeChangeComplete', (url) => console.log(url));
 
-const PageContainer = ({ title , children }) => (
+const PageContainer = ({ title, children }) => (
   <div>
     <Head>
       <title>{title || 'ギルド物語2キャラクター画像ジェネレーター'}</title>
