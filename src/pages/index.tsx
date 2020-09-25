bb7d4d8b4b33726a3a6199112c425b499bde3c4b
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-09-25 19:29:57 +0900

    move preview component to bottom, add title/description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,15 @@ import Form from '~/components/form';
 
 const IndexPage = () => (
   <PageContainer>
+    <h2>冒険者ギルド物語2 キャラクター画像ジェネレーター</h2>
+    <p>
+      種族・現職業・全職業・個性を選択すると画像がダウンロードできるほか、
+      「吸忍(ロ)竜」などのテキストがコピーできるツールです。
+    </p>
     <CharactorContainer.Provider>
+      <Form />
       <Canvas />
       <Output />
-      <Form />
     </CharactorContainer.Provider>
   </PageContainer>
 );
